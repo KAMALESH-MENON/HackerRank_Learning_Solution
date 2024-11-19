b6bb23aed45c190c04db12cb45013fc87536cfe2
KAMALESH <75680480+KAMALESH-MENON@users.noreply.github.com>
2024-11-19 14:25:38 +0530

    Day 7

---
+++ exceptions.py
@@ -0,0 +1,12 @@
+n = int(input())
+for i in range(n):
+    a, b = map(str, input().split())
+
+    try:
+        print (int(a)//int(b))
+    except ZeroDivisionError as e:
+        print( "Error Code:",e)
+    except ValueError as v:
+        print( "Error Code:",v)
+    
+    
